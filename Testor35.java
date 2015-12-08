03fa4137bbc8b158a981f60ed333ff9af45e9a78
Jay D. Foer <dragontbone@gmail.com>
2015-12-08 11:51:42 +0000

    solution or35

---
+++ Testor35.java
@@ -0,0 +1,30 @@
+package CodingBat;
+
+public class Testor35 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	
+	public boolean or35(int n) {
+		  if (n%5 ==0 && n%3 == 0){
+		     return true;
+		  }
+		  else if (n % 5 ==0){
+		      return true;
+		  }
+		  
+		  else if (n % 3 ==0){
+		      return true;
+		  }
+		  
+		  else{
+		     return false;
+		  }
+
+		}
+
+
+}
