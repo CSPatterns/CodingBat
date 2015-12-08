743464925f275602bf792cb160e0930c6e95c4ab
Jay D. Foer <dragontbone@gmail.com>
2015-12-08 15:19:17 +0000

    attempt2 startHi

--- TestStartHi.java
+++ TestStartHi.java
@@ -4,8 +4,11 @@ public class TestStartHi {
 
 	public static void main(String[] args) {
 		
-		String s = "hiho";
+		String s = "hi ho";
 		System.out.println(s.substring(0, 2));
+		if (s.substring(0,2) == "hi"){
+			System.out.println("true");
+		}
 		
 		TestStartHi t = new TestStartHi();
 		System.out.println(t.startHi(s));
@@ -14,8 +17,11 @@ public class TestStartHi {
 	
 	public boolean startHi(String str) {
 
-		  if((str.substring(0,2)) == "hi") 
-		  return true;
+		  if (str.length() < 2) 
+		      return false;
+		  
+		  else if (str.substring(0,2) == "hi")
+		            return true;
 		  
 		  else
 		  return false;
